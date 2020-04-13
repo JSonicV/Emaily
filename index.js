a05bb045f18caf72acb67769088868cf65dc610e
Pier <pier@lamalab.it>
2020-04-14 00:27:45 +0200

    finished auth flow

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
-// https://lit-meadow-98814.herokuapp.com/ ====> remote webapp
-// https://git.heroku.com/lit-meadow-98814.git ====> remote repository
-const express = require('express');
-const app = express();
+// remote webapp ====> https://lit-meadow-98814.herokuapp.com/ 
+// remote repository ====> https://git.heroku.com/lit-meadow-98814.git 
 
-app.get('/', (req, res) => {
-    res.send({ bye: 'buddy' });
-});
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+const UserModel = require('./models/User');
+const authRouter = require('./routes/authRoutes');
+
+
+try {
+    mongoose.connect(keys.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+} catch (err) {
+    console.log('DB connection error:', err);
+}
+
+
+const app = express();
+app.use(cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(authRouter);
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`---- Server listening on port ${PORT}..`);
-})
+});
+
+
+
---
+++ services/passport.js
@@ -0,0 +1,34 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
+const { googleClientID, googleClientSecret } = require('../config/keys');
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+    const user = await User.findById(id);
+    done(null, user);
+});
+
+passport.use(
+    new GoogleStrategy({
+        clientID: googleClientID,
+        clientSecret: googleClientSecret,
+        callbackURL: '/auth/google/callback'
+    }, async (accessToken, refreshToken, profile, done) => {
+        const user = await User.findOne({ googleId: profile.id });
+        if (!user) {
+            new User({ googleId: profile.id }).save()
+                .then(user => done(null, user))
+                .catch(err => console.log(err));
+        } else {
+            done(null, user);
+        }
+    })
+);
+
+module.exports = passport;
